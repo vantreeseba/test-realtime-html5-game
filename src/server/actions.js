de3c45368235b4e27a40b6a79d012b5a09de6237
Benjamin Van Treese <vantreeseba@gmail.com>
2017-02-12 03:44:58 -0600

    trying it with desteer on the server:

--- src/server/actions.js
+++ src/server/actions.js
@@ -1,8 +1,17 @@
+var desteer = require('./desteer.js');
+
 module.exports = (primus) => {
   let players = [];
   let lastProcessedMessage = {};
   let speed = 0.1;
   let physics = 15;
+
+  players['desteer-entity'] = {
+    x: 500,
+    y: 100,
+    vel: {x: 0, y:0},
+    id: 'desteer-entity'
+  };
 
   let routes = {
     keyEvent(spark, data) {
@@ -61,12 +70,26 @@ module.exports = (primus) => {
 
   setInterval(function(){
     let pArray = [];
+    let test = players['desteer-entity'];
+
+    let testPos = new desteer.de.math.Vector(test.x, test.y);
+
     Object.keys(players).forEach(id => {
       let player = players[id];
 
       player.x += player.vel.x;
       player.y += player.vel.y;
       pArray.push(player);
+
+      if(player.id !== 'desteer-entity'){
+        let targetPos = new desteer.de.math.Vector(player.x, player.y);
+
+        var vel = desteer.de.steer.behaviors.seek(testPos, targetPos, 1);
+        test.x += vel.x;
+        test.y += vel.y;
+        test.vel.x = vel.x;
+        test.vel.y = vel.y;
+      }
     });
 
     primus.write({type:'players', players: pArray, inputs: lastProcessedMessage});
